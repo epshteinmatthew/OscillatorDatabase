f8e5e0c89d81d64c0b02d4afbd9cbc0090277898
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2025-08-02 19:05:23 -0700

    Update edit.py

--- operations/edit.py
+++ operations/edit.py
@@ -39,7 +39,7 @@ def edit(filepath_to_change, replacement_ant_string):
             json.dump(data, f)
         with open(filepath_to_change, "w") as fp:
             fp.write(replacement_ant_string)
-        with open("checksum", "w") as ch:
+        with open("checksum", "w+") as ch:
             ch.write(( int(ch.read())+1).__str__())
         print("added model")
         return
